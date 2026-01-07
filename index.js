92b6ec6f62076e8e2fdffae28875d7d54f0301ac
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-01-07 10:02:39 +0100

    fix: use Azure PORT

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ const server = http.createServer((req, res) => {
     }
   });
 });
+const port = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log("Server running on port", port);
 });
+
+/*server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});*/
+
